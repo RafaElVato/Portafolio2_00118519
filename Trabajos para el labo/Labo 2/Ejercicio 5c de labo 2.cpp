318a35d5deeaa54b78366dc9071ade5cfabcb463
RafaElVato <00118519@uca.edu.sv>
2019-09-19 12:24:35 -0600

    carpetas modificadas

---
+++ Trabajos para el labo/Labo 2/Ejercicio 5c de labo 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void torreOrigen(int x, int y, int z){
+    if(x < y){
+
+    }
+
+}
+
+void torreAux(int a, int b, int c){
+
+
+
+
+}
+
+void torreDestino(int prim, int segun, int terc){
+    if(prim)
+
+}
+
+int main(){
+    int disco1 = 1;
+    int disco2 = 10;
+    int disco3 = 25;
+
+
+
+    return 0;
+}
---
+++ Trabajos_de_portafolio/Novena actividad/Novena actividad de portafolio.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+//ejercicio 1 donde se muestra la pila completamente vacia
+/*
+int main(){
+    stack<int> s;
+    bool empty();
+
+    s.push(85);
+    s.push(22);
+    s.push(45);
+    s.pop();
+    s.pop();
+    s.pop();
+
+    if(s.empty() == true){
+    cout << "Esta vacio" << endl;
+    }
+    else{
+    cout << "Tiene valores dentro" << endl;
+    }
+
+
+return 0;
+}
+*/
+//ejercicio 2 para imprimir el ultimo valor que esta ingresado en la pila
+int main(){
+    stack<int> s;
+    bool empty();
+
+    s.push(24);
+    s.push(2);
+    s.push(34);
+    s.push(78);
+
+    if(s.empty() == true){
+        cout << "Esta vacia" << endl;
+    }
+    else{
+        cout << "Tiene valores dentro" << endl;
+    }
+
+    s.pop();
+    s.pop();
+    s.pop();
+
+    cout << s.top() << endl;
+    s.push(2);
+    s.push(34);
+    s.push(78);
+
+
+
+return 0;
+
+}
